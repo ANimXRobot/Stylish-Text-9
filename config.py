786a03474d2fa2784566ec8da159abae1b2c782a
ANim X <91423171+ANimXRobot@users.noreply.github.com>
2021-10-11 08:54:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ import os
 
 class Config(object):
 
-      BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-      API_ID = int(os.environ.get("API_ID", 12345))
-      API_HASH = os.environ.get("API_HASH")
-      OWNER_ID = int(os.environ.get("OWNER_ID"))
-
+      BOT_TOKEN=2050532251:AAF19SVrrk-dzt20g80gAsbWjTWbn-cxgQE
+      API_ID=7756756
+      API_HASH=965a176a30869a7adfd770546dbcb3c6
+      OWNER_ID=1501128718
